539a8f9fdf2e16ef5f7942f8b417040ac7b59da0
Zheng, Lei <realthunder.dev@gmail.com>
2021-12-18 13:40:42 +0800

    Part: fix SubShapeBinder relative placement update

--- src/Mod/Part/App/SubShapeBinder.cpp
+++ src/Mod/Part/App/SubShapeBinder.cpp
@@ -299,9 +299,11 @@ void SubShapeBinder::update(SubShapeBinder::UpdateOption options) {
                 }
             }else{
                 Base::Matrix4D mat;
+                resolvedSub += getNameInDocument();
+                resolvedSub += ".";
                 auto sobj = resolved->getSubObject(resolvedSub.c_str(),0,&mat);
                 if(sobj!=this) {
-                    FC_LOG(getFullName() << " skip invalid parent " << resolved->getFullName() 
+                    FC_WARN(getFullName() << " skip invalid parent " << resolved->getFullName() 
                             << '.' << resolvedSub);
                 }else if(_Version.getValue()==0) {
                     // For existing legacy SubShapeBinder, we use its Placement
