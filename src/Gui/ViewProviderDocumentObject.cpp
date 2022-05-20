2bc294d958d7821fd475776112de14adf22c277d
Zheng, Lei <realthunder.dev@gmail.com>
2022-05-20 21:25:40 +0800

    Gui: lower debug log level

--- src/Gui/ViewProviderDocumentObject.cpp
+++ src/Gui/ViewProviderDocumentObject.cpp
@@ -1024,7 +1024,7 @@ bool ViewProviderDocumentObject::isShowable(bool refresh) {
         return showable;
 
     _Showable = showable;
-    FC_LOG((_Showable?"showable ":"not showable ") << obj->getNameInDocument());
+    FC_TRACE((_Showable?"showable ":"not showable ") << obj->getNameInDocument());
 
     // showability changed
 
