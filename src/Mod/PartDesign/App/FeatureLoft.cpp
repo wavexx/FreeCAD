494e1217b7516604570b194f793866e5238b4746
Zheng, Lei <realthunder.dev@gmail.com>
2022-10-24 08:54:19 +0800

    PartDesign: fix handle of free edges in Loft sections
    Fixes #604
    @m-sundman

--- src/Mod/PartDesign/App/FeatureLoft.cpp
+++ src/Mod/PartDesign/App/FeatureLoft.cpp
@@ -99,6 +99,11 @@ Loft::getSectionShape(const char *name,
     }
     auto compound = TopoShape().makECompound(shapes, "", false);
     auto wires = compound.getSubTopoShapes(TopAbs_WIRE);
+    auto edges = compound.getSubTopoShapes(TopAbs_EDGE, TopAbs_WIRE); // get free edges and make wires from it
+    if (edges.size()) {
+        auto extra = TopoShape().makEWires(edges).getSubTopoShapes(TopAbs_WIRE);
+        wires.insert(wires.end(), extra.begin(), extra.end());
+    }
     const char *msg = "Loft: Sections need to have the same amount of wires or vertices as the base section";
     if (!wires.empty()) {
         if (expected_size && expected_size != wires.size())
