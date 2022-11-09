63d512be6f09410e64c83cc8b75be4fd0f3a4407
Zheng, Lei <realthunder.dev@gmail.com>
2022-11-09 10:20:20 +0800

    Part: try to return primary sub shape in Feature::getTopoShape()

--- src/Mod/Part/App/PartFeature.cpp
+++ src/Mod/Part/App/PartFeature.cpp
@@ -1140,6 +1140,23 @@ TopoShape Feature::getTopoShape(const App::DocumentObject *obj, const char *subn
     auto shape = _getTopoShape(obj, subname, needSubElement, &mat, 
             powner, resolveLink, noElementMap, hiddens, lastLink);
 
+    if (needSubElement && shape.shapeType(true) == TopAbs_COMPOUND) {
+        if (shape.countSubShapes(TopAbs_SOLID) == 1)
+            shape = shape.getSubTopoShape(TopAbs_SOLID, 1);
+        else if (shape.countSubShapes(TopAbs_COMPSOLID) == 1)
+            shape = shape.getSubTopoShape(TopAbs_COMPSOLID, 1);
+        else if (shape.countSubShapes(TopAbs_FACE) == 1)
+            shape = shape.getSubTopoShape(TopAbs_FACE, 1);
+        else if (shape.countSubShapes(TopAbs_SHELL) == 1)
+            shape = shape.getSubTopoShape(TopAbs_SHELL, 1);
+        else if (shape.countSubShapes(TopAbs_EDGE) == 1)
+            shape = shape.getSubTopoShape(TopAbs_EDGE, 1);
+        else if (shape.countSubShapes(TopAbs_WIRE) == 1)
+            shape = shape.getSubTopoShape(TopAbs_WIRE, 1);
+        else if (shape.countSubShapes(TopAbs_VERTEX) == 1)
+            shape = shape.getSubTopoShape(TopAbs_VERTEX, 1);
+    }
+
     Base::Matrix4D topMat;
     if(pmat || transform) {
         // Obtain top level transformation
