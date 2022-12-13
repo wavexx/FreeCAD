bbae415ff1bd8f6c11c0cff752706658b6e47ba4
Zheng, Lei <realthunder.dev@gmail.com>
2022-12-13 11:11:17 +0800

    Base: fix restoring base64 encoded file embedded in xml
    Fixes #653
    @snelweg

--- src/Base/Reader.cpp
+++ src/Base/Reader.cpp
@@ -400,6 +400,7 @@ std::istream &Base::XMLReader::beginCharStream(bool base64) {
     // child elements and character content at the same time.
     if (ReadType == StartElement) {
         CharacterOffset = 0;
+        Characters.clear();
         read();
     } else if (ReadType == StartEndElement) {
         // If we are current at a self closing element, just leave the offset
@@ -571,7 +572,8 @@ void Base::XMLReader::characters(const   XMLCh* const chars, const XMLSize_t len
 
     // We only capture characters when some one wants it
     if(CharacterOffset>=0) {
-        Characters = StrXUTF8(chars).c_str();
+        Characters.erase(Characters.begin(), Characters.begin()+CharacterOffset);
+        Characters += StrXUTF8(chars).c_str();
         CharacterOffset = 0;
     }
 }
